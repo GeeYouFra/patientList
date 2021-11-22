c652175731d909fe830364ca6b27d11808ee3578
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2021-11-22 18:11:39 +0200

    Second commit

---
+++ src/main/java/com/example/PatientList/domain/Message.java
@@ -0,0 +1,18 @@
+package com.example.PatientList.domain;
+
+public class Message {
+
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+	
+}
