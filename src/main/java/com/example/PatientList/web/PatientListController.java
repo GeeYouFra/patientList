d9687780256aa3f356c131f61929dc74272da0aa
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2021-11-24 14:21:36 +0200

    Third commit

--- src/main/java/com/example/PatientList/web/PatientListController.java
+++ src/main/java/com/example/PatientList/web/PatientListController.java
@@ -9,58 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.PatientList.domain.Message;
 
 @Controller
 
 public class PatientListController {
 	
-	@GetMapping("/form")
-	public String formAccess(Model model) {
-		model.addAttribute("message", new Message());
-		return "form";
-	}
-	
-	@GetMapping("/result")
-	public String resultAccess(Model model) {
-		return "result";
-	}
-	
-	@PostMapping("/hello")
-	public String greetingSubmit(@ModelAttribute Message msg, Model model) {
-		model.addAttribute("message", msg);
-		return "result";
+	@RequestMapping(value="/index", method=RequestMethod.POST)
+	public String showIndex() {
+		return "index";
 	}
 
-	@RequestMapping("/add")
-	public String addHello(@RequestParam(name = "firstname") String firstName) {
-		return "Hello " + firstName;
-	}
-
-	@RequestMapping("/greeting")
-	public String addHelloAgain(@RequestParam(name = "name") String name, Model model) {
-		model.addAttribute("name", name);
-		return "hello";
-	}
-
-	@RequestMapping("/message")
-	public String messages(Model model) {
-		Message message1 = new Message();
-		message1.setContent("Moon");
-		Message message2 = new Message();
-		message2.setContent("Uranus");
-		Message message3 = new Message();
-		message3.setContent("Mars");
-
-		List<Message> messages = new ArrayList<Message>();
-		messages.add(message1);
-		messages.add(message2);
-		messages.add(message3);
-
-		model.addAttribute("messages", messages);
-		return "messagelist";
-	}
 }
